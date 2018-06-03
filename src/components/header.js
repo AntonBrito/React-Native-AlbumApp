e8a520d42a8f06183a798fb6966bba3809ba5084
AntonBrito <tonybrito85@gmail.com>
2018-06-03 11:03:16 -0400

    Setup react-nat envirom, Created header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// import libraries for making new component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+// Make the component avaible to other parts of the app
+export default Header;
