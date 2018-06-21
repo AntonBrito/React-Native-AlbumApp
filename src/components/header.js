abe0d998c25e51b9e2f6f19c4eb990420f92a60b
AntonBrito <tonybrito85@gmail.com>
2018-06-21 01:25:17 -0400

    adding header style

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,29 @@
 // import libraries for making new component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  return <Text>Albums!</Text>;
+    const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>;
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15
+  },
+  textStyle: {
+      fontSize: 20
+  }
 };
 
 // Make the component avaible to other parts of the app
